edf4103185c350ee15fe5c1e173d5696eaf5fd5b
Bartek <bartekkedra@onet.pl>
2025-05-25 23:14:12 +0200

    add class CampOrganisation

---
+++ src/main/java/com/example/testingexercise/TestingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingexercise/TestingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
